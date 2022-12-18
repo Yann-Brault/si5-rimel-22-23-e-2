5d9629ecf412319344b5e0853c1311b95a467157
Nicolas-Fern <71392223+Nicolas-Fern@users.noreply.github.com>
2022-12-18 10:57:31 +0100

    Print paternals sorted

--- commit_fetch.py
+++ commit_fetch.py
@@ -1,15 +1,18 @@
+import json
+
 from git import Repo
+from git import Actor
 import git
 import re
 
-repository = Repo.init('./')
+repository = Repo.init('../selene-22-23-soa-selene-22-23-c')
 
 # print(repository.git.log("--patch", "8d9fd97773fa90a568024151126abe1399a48c7d"))
 # print(repository.git.show("8d9fd97773fa90a568024151126abe1399a48c7d"))
 
 # repository.git.log()
 
-commits = {}
+commits = dict()
 
 # commits = {
 #       commit_id: {
@@ -23,9 +26,9 @@ commits = {}
 # }
 
 # git show ed5927e45d0a5cb60251b84e7a1faa5ed1a328b5
-
+# Names of all the commits that modify environnment variables
+names = []
 for commit in repository.iter_commits():
-    # print(commit, commit.author, commit.committed_date)
     commit_name = commit.__str__()
 
     for commit_file in commit.stats.files.keys():
@@ -40,7 +43,7 @@ for commit in repository.iter_commits():
                 print("File ", commit_file,
                       " was deleted in the commit ", commit_name)
                 continue
-
+            names.append(commit_name)
             for line in result.splitlines():
                 if len(line) > 0:
                     if line[0] == '+' or line[0] == '-':
@@ -63,17 +66,40 @@ for commit in repository.iter_commits():
                                 commits[commit_name]['deletion'].append(
                                     {name: {'file': commit_file, 'value': value}})
 
-# print(commits)
-    var_utils = {}
-    if commit_name in commits.keys():
-        for commit_file in commit.stats.files.keys():
-            try:
-                result = repository.git.show(commit_name)
-            except:
-                print(
-                    f"file {commit_file} was deleted in the commit {commit_name}")
-                continue
-            # var_utils[commit_name] = {"file": commit_file, "result": result}
-            print(
-                f"commit_name : {commit_name}, file : {commit_file}, result : {result}")
+#print(commits)
+
+# Get Paternity users
+paternals = {}
+for name in names:
+    #We get the attributes names coz 1 user can use different email address but the same Username
+    author = commits[name]['author'].name
+    if author in paternals.keys():
+        paternals[author] = paternals[author] + 1
+    else:
+        paternals[author] = 1
+
+sorted_paternals = {k: v for k, v in sorted(paternals.items(), key=lambda item: item[1], reverse=True)}
+print(sorted_paternals)
+# # Writing to sample.json
+# with open("commits.json", "w") as outfile:
+#     outfile.write(json_object)
+#
+#
+#     var_utils = dict()
+#     if commit_name in commits.keys():
+#         for commit_file in commit.stats.files.keys():
+#             try:
+#                 result = repository.git.show(commit_name)
+#             except:
+#                 print(
+#                     f"file {commit_file} was deleted in the commit {commit_name}")
+#                 continue
+#             var_utils[commit_name] = {"file": commit_file, "result": result}
+#             print(
+#                 f"commit_name : {commit_name}, file : {commit_file}, result : {result}")
 # print(var_utils)
+# # json_object = json.dumps(var_utils)
+# #
+# # # Writing to sample.json
+# # with open("var_utils.json", "w") as outfile:
+# #     outfile.write(json_object)
